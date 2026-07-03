881613ce7f2e84e9cb2d82c727eff12b3e1e9ddc
isaave <isabellaavelina84626@gmail.com>
2026-07-02 22:02:07 -0300

    Initial Commit

---
+++ LLock/LLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLockApp.swift
+//  LLock
+//
+//  Created by Isabella Avelina on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
